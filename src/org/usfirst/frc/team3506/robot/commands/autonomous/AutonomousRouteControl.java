a1024057a0cf20b4fe7a088318e75112bf28ff00
Sam Perlmutter <sam.perlmutter227@gmail.com>
2017-03-08 10:44:51 -0500

    - Added class to make it easier to create/test a simple driving auto (untested)

---
+++ src/org/usfirst/frc/team3506/robot/commands/autonomous/AutonomousRouteControl.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team3506.robot.commands.autonomous;
+
+import org.usfirst.frc.team3506.robot.commands.drivetrain.DriveStraightDistanceAtPower;
+import org.usfirst.frc.team3506.robot.commands.drivetrain.PointTurnDegreesAtPowerCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class AutonomousRouteControl extends CommandGroup {
+
+	boolean moveStepEnable[];
+	double moveType[];
+	double moveSpeed[];
+	double moveAngle[];
+	double moveDistance[];
+	int numSteps;
+	NetworkTable table;
+	String tableName = "prefs";
+
+	public AutonomousRouteControl() {
+		table = NetworkTable.getTable(tableName);
+		
+		moveStepEnable = table.getBooleanArray("moveStepEnable", new boolean[] {});
+		moveType = table.getNumberArray("moveType", new double[] {});
+		moveSpeed = table.getNumberArray("moveSpeed", new double[] {});
+		moveAngle = table.getNumberArray("moveAngle", new double[] {});
+		moveDistance = table.getNumberArray("moveDistance", new double[] {});
+		numSteps = moveStepEnable.length;
+	}
+
+	// Autonomous Sequencer: This for loop will cycle through each
+	// Autonomous step as indicated by the stepIndex;AKA the array element.
+	// As each step completes, it increments through all the steps. If the
+	// step is not enabled, it skips the case statement.
+	public void runSequencer() {
+		for (int i = 0; i < numSteps; i++) {
+			if (moveStepEnable[i]) {
+				switch ((int) moveType[i]) {
+					case 1:
+						addSequential(new DriveStraightDistanceAtPower(moveSpeed[i], moveDistance[i]));
+					case 2:
+						addSequential(new PointTurnDegreesAtPowerCommand(moveAngle[i], moveSpeed[i]));
+				}
+				System.out.println("Step " + i + " completed.");
+			} else {
+				System.out.println("Step " + i + " is not enabled.");
+			}
+		}
+	}
+}
